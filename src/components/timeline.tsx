1b34ee930f9b72be65caa1fb49255c84343b44ea
Draktors <hespelenzo@gmail.com>
2022-10-17 23:10:48 +0200

    fix contact display on mobile

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -24,11 +24,11 @@ export const Timeline: FC<TimelineProps> = ({
       <ul className="timeline">
         {timelineData.map((datum, i) => (
           <li key={i} className="mb-8 relative pl-12 animate-timeline">
-            <div className="flex items-center mb-2">
+            <div className="flex sm:items-center mb-2">
               <h3 className=" text-xl lg:text-2xl font-semibold mr-2">
                 {datum.title}
               </h3>
-              <span className="text-lg lg:text-xl text-yellow font-semibold">
+              <span className="text-lg lg:text-xl text-yellow font-semibold whitespace-nowrap">
                 {parseDate(datum.startDate, datum.endDate)}
               </span>
             </div>
